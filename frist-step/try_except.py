f06496cfbc5c8dbd93987ba172c8278335bf3a56
yangchaoben <842293890@qq.com>
2016-01-05 16:37:47 +0800

    try_expect2

--- frist-step/try_except.py
+++ frist-step/try_except.py
@@ -1,9 +1,26 @@
-import sys
+# import sys
+# try:
+# 	s=input('Enter something-->')
+# except EOFError:
+# 	print('\nWhy did you do an EOF on me?')
+# 	sys.exit()
+# except:
+# 	print("\nSome error/exception occurred.")
+# print('Done')
+
+class ShortInputException(Exception):
+	def __init__(self,length,atleast):
+		Exception.__init__(self)
+		self.length=length
+		self.atleast=atleast
 try:
-	s=raw_input('Enter something-->')
+	s=input('Enter something-->')
+	if len(s)<3:
+		raise ShortInputException(len(s),3)
 except EOFError:
-	print('\nWhy did you do an EOF on me?')
-	sys.exit()
-except:
-	print("\nSome error/exception occurred.")
-print('Done')
+    print('\nWhy did you do an EOF on me?')
+except ShortInputException:
+    print('ShortInputException: The input was of length %d, \
+          was expecting at least')
+else:
+    print('No exception was raised.')
