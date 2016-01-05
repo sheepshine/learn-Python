482a3ea9f656e53bbed7da217e214d23ee58f9c9
yangchaoben <842293890@qq.com>
2016-01-05 16:09:59 +0800

    pickle

--- frist-step/pickling.py
+++ frist-step/pickling.py
@@ -1,3 +1,12 @@
-import cPickle as p
+#coding:utf-8
+import pickle as p
 shoplistfile="shoplist.data"
 shoplist=['apple','mango','carrot']
+f=open(shoplistfile,'wb')
+p.dump(shoplist,f)
+f.close()
+del shoplist
+f=open(shoplistfile)
+#编码问题
+storelist=p.load(f)
+print(storelist)
